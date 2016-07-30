fd603a32cfc2c206b5592a03cac462c1445a3f37
Nishant Rodrigues <nishantjr@gmail.com>
2016-07-30 19:43:45 +0530

    Gulpfile: When serving the site, watch for changes to source and recompile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,17 +2,23 @@
 
 const gulp = require('gulp'),
       connect = require('gulp-connect'),
+      watch = require('gulp-watch'),
       sass = require('gulp-sass')
 
 const sassOptions = { includePaths: ['node_modules/bootstrap/scss/'] }
 
-gulp.task('sass', function () {
+gulp.task('build-sass', function () {
    return gulp.src('./sass/**/*.scss')
      .pipe(sass(sassOptions).on('error', sass.logError))
      .pipe(gulp.dest('./.build/www/css/'));
 });
 
-gulp.task('build', ['sass'], function () {
+gulp.task('build-src', function () {
+   return gulp.src(['src/**/*'])
+     .pipe(gulp.dest('.build/www/'));
+});
+
+gulp.task('build', ['build-src', 'build-sass'], function () {
    return gulp.src(['src/**/*'])
      .pipe(gulp.dest('.build/www/'));
 });
@@ -21,6 +27,9 @@ gulp.task('serve', ['build'], (cb) => {
     connect.server({
         root: '.build/www/'
     });
+
+    watch('src/**/*', function() { gulp.start('build-src') })
+    watch('sass/**/*', function() { gulp.start('build-sass') })
 })
 
 gulp.task('default', ['serve'])
