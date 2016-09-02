a6b84e81091107416e02c6e28f421c2205be4bd6
Nishant Rodrigues <nishantjr@gmail.com>
2016-09-02 15:27:30 -0500

    Gulpfile: Process markdown files in src/ and convert them to html

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const gulp = require('gulp')
+const markdown = require('gulp-markdown-it')
 
 gulp.task('build', function() {
     return gulp
         .src('src/**/*')
+        .pipe(markdown())
         .pipe(gulp.dest('.build/www/'))
 })
